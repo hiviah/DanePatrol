13aa4edafa722b8d49e879843cf1c9870e673ae6
Ondrej Mikle <ondrej.mikle@nic.cz>
2012-09-09 03:00:10 +0200

    Cleaned out test code

--- plugin-source/TLSAfetcher/TLSAfetcherAPI.cpp
+++ plugin-source/TLSAfetcher/TLSAfetcherAPI.cpp
@@ -224,14 +224,6 @@ FB::VariantMap TLSAfetcherAPI::fetchTLSA(const std::string& fqdn, int port)
     jsResult["tlsa"] = tlsaList;
     jsResult["dnssec"] = dnssecStatus;
     
-    std::vector<ResolvedTLSA> sl;
-    FB::VariantList vl = FB::make_variant_list(tlsaList);
-    vl = FB::make_variant_list(sl);
-    FB::variant v1 = vl;
-    typedef std::map<std::string, FB::variant> StringVariantMap; 
-    FB::VariantMap svm = FB::variant_map_of<std::string>("a",1)("b","2")("c",3.4); 
-    v1 = svm;
-    
     return jsResult;
 }
 

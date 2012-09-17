9a89f9a3b5d507ad77a06fe7fe92e52077ad9c78
Ondrej Mikle <ondrej.mikle@nic.cz>
2012-09-17 15:05:22 +0200

    Header structure mockup for cert checking

--- plugin-source/TLSAfetcher/DANEAlgorithm.h
+++ plugin-source/TLSAfetcher/DANEAlgorithm.h
@@ -7,8 +7,45 @@
 #ifndef H_DANEAlgorithm
 #define H_DANEAlgorithm
 
+#include <string>
+#include <vector>
+
+class Certificate
+{
+public:
+
+    /*! Construct certificate from DER-encoded data. */
+    Certificate(const std::string& derData);
+
+    /*! Return cert as DER-encoded */
+    const std::string& asDer() const
+        { return m_derData; }
+
+    /*! Return ASN.1 DER-encoded SubjectPublicKeyInfo structure. */
+    const std::string& spki() const
+        { return m_spki; }
+
+protected:
+
+    /*! DER-encoded cert */
+    std::string m_derData;
+
+    /*! DER-encoded ASN.1 structure of SPKI. */
+    std::string m_spki;
+};
+
+/*! Typedef for certificate chain. */
+typedef std::vector<Certificate> CertChain;
+
+/*! Implementation of DANE/TLSA checking algorithm for a site. */
 class DANEAlgorithm
 {
+public:
+
+protected:
+
+    /*! Certificate chain of site to be checked. */
+    CertChain m_certChain;
 };
 
 #endif /* H_DANEAlgorithm */

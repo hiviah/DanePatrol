16313c6f485d690f5726fd994c4db42296233d89
Ondrej Mikle <ondrej.mikle@nic.cz>
2012-09-27 15:51:11 +0200

    Test and instructions for using generated TLSA record on local nameserver

---
+++ zone/unbound_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from unbound import ub_ctx, ub_version
+from binascii import hexlify
+
+RR_TYPE_TLSA = 52
+
+print "Unbound version:", ub_version()
+
+u = ub_ctx()
+u.add_ta_file("keys.tlsa-test")
+u.set_fwd("127.0.0.1")
+
+for fqdn in ("_443._tcp.tlsa-test",):
+	s, r = u.resolve(fqdn, RR_TYPE_TLSA)
+	print "fqdn: %s, status: %s, rcode: %s, secure: %s, bogus: %s, why_bogus: %s" % \
+	    (fqdn, s, r.rcode, r.secure, r.bogus, r.why_bogus)
+        if r.havedata:
+            print [hexlify(rr) for rr in r.data.data]
+	

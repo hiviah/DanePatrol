f6eefce61d190f46aefc0bf74c05531f3452435e
Ondrej Mikle <ondrej.mikle@nic.cz>
2012-10-31 17:06:00 +0100

    Added library dependencies for TLSAFetcher Mac CMake files

--- plugin-source/TLSAfetcher/Mac/projectDef.cmake
+++ plugin-source/TLSAfetcher/Mac/projectDef.cmake
@@ -28,6 +28,42 @@ set(LOCALIZED "Mac/bundle_template/Localized.r")
 add_mac_plugin(${PROJECT_NAME} ${PLIST} ${STRINGS} ${LOCALIZED} SOURCES)
 
 # add library dependencies here; leave ${PLUGIN_INTERNAL_DEPS} there unless you know what you're doing!
-target_link_libraries(${PROJECT_NAME}
+#target_link_libraries(${PROJECT_NAME}
+#    ${PLUGIN_INTERNAL_DEPS}
+#    )
+
+# set header file directories
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/../../libs/openssl/include
+                    ${CMAKE_CURRENT_SOURCE_DIR}/../../libs/ldns/include
+                    ${CMAKE_CURRENT_SOURCE_DIR}/../../libs/unbound/include)
+
+# set static library paths
+add_library(unbound STATIC IMPORTED)
+set_property(TARGET unbound PROPERTY IMPORTED_LOCATION
+             ${CMAKE_CURRENT_SOURCE_DIR}/../../libs/unbound/lib/libunbound.a)
+
+add_library(ldns STATIC IMPORTED)
+set_property(TARGET ldns PROPERTY IMPORTED_LOCATION
+             ${CMAKE_CURRENT_SOURCE_DIR}/../../libs/ldns/lib/libldns.a)
+
+add_library(ssl STATIC IMPORTED)
+set_property(TARGET ssl PROPERTY IMPORTED_LOCATION
+             ${CMAKE_CURRENT_SOURCE_DIR}/../../libs/openssl/lib/libssl.a)
+
+add_library(crypto STATIC IMPORTED)
+set_property(TARGET crypto PROPERTY IMPORTED_LOCATION
+             ${CMAKE_CURRENT_SOURCE_DIR}/../../libs/openssl/lib/libcrypto.a)
+
+# add library dependencies here; leave ${PLUGIN_INTERNAL_DEPS} there unless you know what you're doing!
+target_link_libraries(${PROJNAME}
     ${PLUGIN_INTERNAL_DEPS}
+    DANECore
+    boost_regex
+    unbound
+    ldns
+    ssl
+    crypto
+
+    #hardening linker flags
+    #-Wl,-z=relro -Wl,-z=now
     )

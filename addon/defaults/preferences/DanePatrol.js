3951e146bd92da93282e3705a9ad3fd6c64863e9
Ondrej Mikle <ondrej.mikle@nic.cz>
2012-10-31 12:34:47 +0100

    Overriding of 'untrusted certificate page' is a default setting now.

--- addon/defaults/preferences/DanePatrol.js
+++ addon/defaults/preferences/DanePatrol.js
@@ -7,6 +7,7 @@ pref("danepatrol.notify.wild", true);
 pref("danepatrol.notify.timeout", 10);
 pref("danepatrol.hosts.ignore", "");
 pref("danepatrol.dane.check", "new_or_had_tlsa");
+pref("danepatrol.dane.override", true);
 
 // See http://kb.mozillazine.org/Localize_extension_descriptions
 pref("extensions.DanePatrol@nic.cz.description", "chrome://danepatrol/locale/DanePatrol.properties");
